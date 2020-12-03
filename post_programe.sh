d546ba49b83cd8e8059d2fd8fe06f4af08392d1b
xyldmc <2590393048@qq.com>
2020-12-03 11:51:55 +0800

    上传代码

--- post_programe.sh
+++ post_programe.sh
@@ -1,10 +1,17 @@
 #!/bin/bash 
-git config --global user.email "2590393048@qq.com"
-git config --global user.name "xyldmc"
-echo "push start"
-cd "https://github.com/xyldmc/Linux_program/"
+#git config --global user.email "2590393048@qq.com"
+#git config --global user.name "xyldmc"
+git remote rm origin
+
+git remote add origin 'https://github.com/xyldmc/Linux_program'
+git pull remote master
 git add .
 git commit -m $1
-git push origin master
-echo "push end"
+git push origin master --force
+if [[ "$?" -eq "0" ]];then
+    echo -e "\033[42;34m push to github success! \033[0m"
+else
+    echo -e "\033[41;30m push to github fail! \033[0m"
+    exit 1
+fi
 
